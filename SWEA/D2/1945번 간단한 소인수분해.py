f3d535243104ba70afac090550aaf06a3d207921
이민규 <lmg386411@gmail.com>
2024-05-02 22:33:59 +0900

    1945번 간단한 소인수분해

---
+++ SWEA/D2/1945번 간단한 소인수분해.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open("input.txt", "r")
+
+nums = [2, 3, 5, 7, 11]
+
+T = int(input())
+for tc in range(1, T+1):
+    cnts = [0] * 5
+    N = int(input())
+    for i in range(len(nums)):
+        while N % nums[i] == 0:
+            N /= nums[i]
+            cnts[i] += 1
+    print(f"#{tc}", *cnts)
